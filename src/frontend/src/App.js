c77a007f65e06bfeec039bd618c6bf8889d17db3
cariocaphil <cariocaphil@gmail.com>
2021-09-22 11:13:26 +0200

    add getAllStudents (cors error for now)

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import { Button } from "antd";
+import {getAllStudents} from './client.js';
 
 function App() {
+  getAllStudents()
+    .then((response) => console.log(response));
   return (
     <div className="App">
         <Button type="primary">Hello</Button>
