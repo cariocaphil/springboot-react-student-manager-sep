502addf3e0653c207bddd16a17e7acd7bbfd69eb
cariocaphil <cariocaphil@gmail.com>
2021-10-16 13:10:26 +0200

    add avatar

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -24,7 +24,28 @@ import {
 const {Header, Content, Footer, Sider} = Layout;
 const {SubMenu} = Menu;
 
+const TheAvatar = ({name}) => {
+    let trim = name.trim();
+    if (trim.length === 0) {
+        return <Avatar icon={<UserOutlined/>}/>
+    }
+    const split = trim.split(" ");
+    if (split.length === 1) {
+        return <Avatar>{name.charAt(0)}</Avatar>
+    }
+    return <Avatar>
+        {`${name.charAt(0)}${name.charAt(name.length-1)}`}
+    </Avatar>
+}
+
 const columns = [
+    {
+        title: '',
+        dataIndex: 'avatar',
+        key: 'avatar',
+        render: (text, student) =>
+            <TheAvatar name={student.name}/>
+    },
     {
         title: 'Id',
         dataIndex: 'id',
