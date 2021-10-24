893b311dc650e6da715abf372182c2754d33665a
cariocaphil <cariocaphil@gmail.com>
2021-10-25 00:48:34 +0200

    add IT test with faker

--- src/test/java/com/example/demo/integration/StudentIT.java
+++ src/test/java/com/example/demo/integration/StudentIT.java
@@ -1,17 +1,65 @@
 package com.example.demo.integration;
 
+import com.example.demo.student.Gender;
+import com.example.demo.student.Student;
+import com.example.demo.student.StudentRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @TestPropertySource(
         locations = "classpath:application-it.properties"
 )
+@AutoConfigureMockMvc
 public class StudentIT {
 
-    @Test
-    void name() {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    private final Faker faker = new Faker();
+
+    @Test
+    void canRegisterNewStudent() throws Exception {
+        // given
+        String name = String.format(
+                "%s %s", faker.name().firstName(),
+                faker.name().lastName());
+        String firstName = String.format(
+                "%s", faker.name().firstName());
+        Student student = new Student(111111L, name
+        , String.format("%s@gmail.com", firstName), Gender.FEMALE);
+
+        // when
+        // when
+        ResultActions resultActions = mockMvc
+                .perform(post("/api/v1/students")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(student)));
+        // then
+        resultActions.andExpect(status().isOk());
+        List<Student> students = studentRepository.findAll();
+        assertThat(students).usingElementComparatorIgnoringFields("id")
+                .contains(student);
     }
 }
