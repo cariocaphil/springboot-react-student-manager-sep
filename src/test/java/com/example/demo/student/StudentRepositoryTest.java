46e3762cffe56c7b785968f6e0e28bce6f643e5e
cariocaphil <cariocaphil@gmail.com>
2021-10-24 17:55:54 +0200

    add a second test

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,6 +13,11 @@ class StudentRepositoryTest {
     @Autowired
     private StudentRepository underTest;
 
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
     @Test
     void itShouldCheckIfStudentExistsEmail() {
         // given
@@ -21,5 +27,17 @@ class StudentRepositoryTest {
         boolean exists = underTest.selectExistsEmail("jamila@gmail.com");
         // then
         assertThat(exists).isTrue();
+    }
+
+    @Test
+    void itShouldCheckIfStudentEmailDoesNotExist() {
+        // given
+        String email = "jamila@gmail.com";
+
+        // when
+        boolean expected = underTest.selectExistsEmail(email);
+
+        // then
+        assertThat(expected).isFalse();
     }
 }
