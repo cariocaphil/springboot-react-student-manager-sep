30da4ba6e10894ce0fd5c9e35cfa8ceba14926d1
cariocaphil <cariocaphil@gmail.com>
2026-09-10 13:51:53 +0200

    renderFieldControl with a switch

--- src/frontend/src/components/students/StudentFormField.tsx
+++ src/frontend/src/components/students/StudentFormField.tsx
@@ -1,4 +1,5 @@
 import { Form, Input, Select } from 'antd';
+import type { ControllerRenderProps } from 'react-hook-form';
 import { Controller, type Control, type FieldErrors } from 'react-hook-form';
 import { validationStatus } from '../../utils/form';
 import type { StudentFormValues } from './studentForm';
@@ -10,6 +11,31 @@ interface StudentFormFieldProps {
   field: StudentFormFieldConfig;
   control: Control<StudentFormValues>;
   errors: FieldErrors<StudentFormValues>;
+}
+
+function renderFieldControl(
+  field: StudentFormFieldConfig,
+  rhfField: ControllerRenderProps<StudentFormValues, keyof StudentFormValues>
+) {
+  switch (field.type) {
+    case 'text':
+      return <Input {...rhfField} placeholder={field.placeholder} />;
+    case 'select':
+      return (
+        <Select
+          placeholder={field.placeholder}
+          value={rhfField.value}
+          onChange={rhfField.onChange}
+          onBlur={rhfField.onBlur}
+        >
+          {field.options.map((option) => (
+            <Option key={option} value={option}>
+              {option}
+            </Option>
+          ))}
+        </Select>
+      );
+  }
 }
 
 function StudentFormField({ field, control, errors }: StudentFormFieldProps) {
@@ -26,22 +52,7 @@ function StudentFormField({ field, control, errors }: StudentFormFieldProps) {
           validateStatus={validationStatus(!!fieldError)}
           help={fieldError?.message}
         >
-          {field.type === 'text' ? (
-            <Input {...rhfField} placeholder={field.placeholder} />
-          ) : (
-            <Select
-              placeholder={field.placeholder}
-              value={rhfField.value}
-              onChange={rhfField.onChange}
-              onBlur={rhfField.onBlur}
-            >
-              {field.options.map((option) => (
-                <Option key={option} value={option}>
-                  {option}
-                </Option>
-              ))}
-            </Select>
-          )}
+          {renderFieldControl(field, rhfField)}
         </Form.Item>
       )}
     />
